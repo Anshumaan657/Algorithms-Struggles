8b83fe89a8a5c6b95fb89d930cf1fe280516f6d2
Anshumaan657 <anshumaansharma270@gmail.com>
2026-06-27 16:47:15 +0530

    Tree and Indirect Tree recursions

---
+++ recursion/04_recursion.cpp
@@ -0,0 +1,16 @@
+// Tree recursion
+#include<iostream>
+using namespace std;
+void fun(int n)
+{
+    if (n>0){
+        cout<<n<<endl;
+        fun(n-1);
+        fun(n-1);
+    }
+}
+int main(){
+    int a = 3;
+    fun(a);
+    return 0;
+}
---
+++ recursion/05_recursion.cpp
@@ -0,0 +1,27 @@
+// Indirect Recusion
+#include<iostream>
+using namespace std;
+void funA(int n);
+void funB(int n);
+//Recursive method A
+void funA(int n){
+    if(n>0)
+    {
+        cout<<n<<endl;
+        funB(n-1);
+    }
+}
+void funB(int n){
+    if(n>1)
+    {
+        cout<<n<<endl;
+        funA(n/2);
+    }
+}
+
+int main(){
+    int n = 20;
+    funA(n);
+
+    return 0;
+}
