b1f2a7ee9d1fa17cf75ab0fa7f1cb7032780501f
Anshumaan657 <anshumaansharma270@gmail.com>
2026-07-18 22:57:30 +0530

    Solution of Finding the GCD of an Array

---
+++ DSA Q's/Find-the-GCD-of-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int findGCD(vector<int>& nums) {
+            int smallest = nums[0];
+            int largest = nums[0];
+    
+            for(int i = 0; i < nums.size(); i++){
+            smallest = min(smallest, nums[i]);
+            largest = max(largest, nums[i]);
+    
+            }
+        
+        return __gcd(smallest, largest);
+      }
+    };
