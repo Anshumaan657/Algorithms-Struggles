ab717791759435c359e74c7270938026b4f2075f
Anshumaan657 <anshumaansharma270@gmail.com>
2026-08-06 23:55:57 +0530

    Solution of the Smallest divisible digit product 1

---
+++ DSA Q's/Smallest-Divisible-Digit-Product-I.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+        int digitProduct(int num) {
+            int product = 1;
+    
+            while (num > 0) {
+                int digit = num % 10;
+                product = product * digit;
+                num = num / 10;
+            }
+    
+            return product;
+        }
+    
+        int smallestNumber(int n, int t) {
+            int num = n;
+    
+            while (true) {
+                int product = digitProduct(num);
+    
+                if (product % t == 0) {
+                    return num;
+                }
+    
+                num++;
+            }
+    
+            return -1;
+        }
+    };
