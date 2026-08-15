8d3c082dea2353ead227c2f405145befa4464622
Anshumaan657 <anshumaansharma270@gmail.com>
2026-08-15 23:09:23 +0530

    Solution of Longest Subsequence With Non-Zero XOR

---
+++ DSA Q's/Longest-Subsequence-With-Non-Zero-XOR.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int longestSubsequence(vector<int>& nums) {
+            int xr = 0;
+            bool hasNonZero = false;
+    
+            for (int num : nums) {
+                xr ^= num;
+    
+                if (num != 0) {
+                    hasNonZero = true;
+                }
+            }
+    
+            if (!hasNonZero) {
+                return 0;
+            }
+    
+            if (xr != 0) {
+                return nums.size();
+            }
+    
+            return nums.size() - 1;
+        }
+    };
